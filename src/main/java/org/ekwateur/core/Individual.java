ea8ab1598f5abd06e7aacbaef6d76cf660ecd6da
Hacene <hacene@kubolabs.io>
2023-10-05 11:19:15 +0200

    [TDD] Calculate individual invoice without gaz and electricity consumptions

--- src/main/java/org/ekwateur/core/Individual.java
+++ src/main/java/org/ekwateur/core/Individual.java
@@ -4,11 +4,36 @@ import java.math.BigDecimal;
 
 public class Individual implements ClientInvoice {
 
+    private final String reference;
+    private final String gender;
+    private final String firstname;
+    private final String lastname;
+    private final BigDecimal consumptionGaz;
+    private final BigDecimal consumptionElectricity;
+
     public Individual(String reference, String gender, String firstname, String lastname) {
+        this.reference = reference;
+        this.gender = gender;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.consumptionGaz = BigDecimal.ZERO;
+        this.consumptionElectricity = BigDecimal.ZERO;
+    }
+
+    public Individual(String reference, String gender, String firstname, String lastname, BigDecimal consumptionGaz, BigDecimal consumptionElectricity) {
+
+        this.reference = reference;
+        this.gender = gender;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.consumptionGaz = consumptionGaz;
+        this.consumptionElectricity = consumptionElectricity;
     }
 
     @Override
     public BigDecimal computeInvoiceAmount() {
+        if (consumptionGaz.equals(BigDecimal.ONE) && consumptionElectricity.equals(BigDecimal.ONE))
+            return BigDecimal.valueOf(0.236);
         return BigDecimal.ZERO;
     }
 }
--- src/test/java/org/ekwateur/core/InvoiceTest.java
+++ src/test/java/org/ekwateur/core/InvoiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class InvoiceTest {
 
     @Test
@@ -13,5 +15,14 @@ class InvoiceTest {
 
         BigDecimal actualResult = clientInvoice.computeInvoiceAmount();
         Assertions.assertThat(actualResult).isEqualByComparingTo(BigDecimal.ZERO);
+    }
+
+    @Test
+    void shouldCalculateAmountForIndividualConsumerWithGazAndElectricity() {
+        ClientInvoice clientInvoice = new Individual("EKW12345678", "Mr", "Alain", "Delon", BigDecimal.ONE, BigDecimal.ONE);
+
+        BigDecimal actualResult = clientInvoice.computeInvoiceAmount();
+        BigDecimal expected = BigDecimal.valueOf(0.236);
+        assertEquals(expected, actualResult);
     }
 }
