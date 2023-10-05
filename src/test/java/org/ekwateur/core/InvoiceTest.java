682a47048a0bc8e57296f60844883ff36754007e
Hacene <hacene@kubolabs.io>
2023-10-05 11:09:10 +0200

    [INIT] Initialize Ekwateur Kata

---
+++ src/test/java/org/ekwateur/core/InvoiceTest.java
@@ -0,0 +1,12 @@
+package org.ekwateur.core;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class InvoiceTest {
+
+    @Test
+    void initTest() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
